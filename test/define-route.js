731d4fd3c975c5be58a426aa8a0ccd8da6f1d068
Joey Lappin <joey@creepysnake.com>
2016-11-09 12:40:09 -0500

    Release 2.1.1: - Fixed a bug wherein event hooks were firing out of order when using RouteMap.extend()

--- test/define-route.js
+++ test/define-route.js
@@ -5,6 +5,8 @@ var chai = require('chai');
 var expect = chai.expect;
 var chaiHttp = require('chai-http');
 var RGTEST = require('./lib/constants');
+var _ = require('lodash');
+var debug = require('debug')('restful-goose:RouteMap');
 
 var app, RouteMap, routeMap;
 
@@ -32,21 +34,67 @@ describe('define route', function() {
   });
 
   it('should override route defaults when extending define route class', function(done) {
-    app = restfulGoose(mongoose);
+    app = restfulGoose(connection);
     RouteMap = restfulGoose.RouteMap;
     routeMap = RouteMap.extend({
-      beforeModel: function(req, res) {
-        res.status(200).json({ cool: true });
+      beforeModel: function(req, res, next) {
+        res.rg.status = 201;
+        next();
+      },
+      afterModel: function(req, res, next) {
+        res.model = { cool: true, _id: new mongoose.Types.ObjectId() };
+        // Stops serializer crash in test        
+        res.model.constructor = connection.model('DefineRouteTest');
+        res.model.toObject = function() {
+          return _.omit(res.model, ['toObject', 'constructor']);
+        };
+        next();
+      },
+      beforeResponse: function(req, res) {
+        res.status(res.rg.status).json(res.model);
       }
     });
     app.defineRoute('DefineRouteTest', routeMap);
     chai.request(app)
       .get('/define-route-tests')
-      .set('Content-Type', 'application/vnd.api+json')
+      .set('Content-Type', 'application/json')
+      .end(function(err, res) {
+        expect(res).to.be.json;
+        expect(res.status).to.equal(201);
+        expect(res.body.cool).to.equal(true);
+        done();
+      });
+  });
+
+  it('should let you access set properties in later hooks when extending RouteMap base class', function(done) {
+    app = restfulGoose(connection);
+    RouteMap = restfulGoose.RouteMap;
+    app.defineRoute('DefineRouteTest', RouteMap.extend({
+      afterModel: function(req, res, next) {
+        debug('after model');
+        res.model = { amazing: true, robots: 'great', _id: new mongoose.Types.ObjectId() };
+        res.model.constructor = connection.model('DefineRouteTest');
+        res.model.toObject = function() {
+          return _.omit(res.model, ['toObject', 'constructor']);
+        };
+        next();
+      },
+      beforeResponse: function(req, res, next) {
+        if (res.model && res.model.amazing === true && res.model.robots === 'great') {
+          _.set(res, 'data.data.attributes.isokay', true);
+        }
+
+        debug('beforeResponse callback');
+        next();
+      }
+    }));
+    chai.request(app)
+      .get('/define-route-tests')
+      .set('Content-Type', 'application/json')
       .end(function(err, res) {
         expect(res).to.be.json;
         expect(res.status).to.equal(200);
-        expect(res.body.cool).to.equal(true);
+        expect(res.body.data.attributes.isokay).to.equal(true);
         done();
       });
   });
